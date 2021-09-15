a1c6625c5fb94e1b009652d559abd75bcfe32a48
상혁 <>
2021-09-15 21:15:28 +0900

    add : nodejs 세팅 테스트

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+var server = http.createServer(app);
+
+app.get('/', function(req, res) { 
+    res.send('root page');
+ });
+
+app.get('/start', function(req, res){
+     res.send('start page'); 
+    }); 
+server.listen(3000, '127.0.0.1', function() { 
+        console.log('Server listen on port ' + server.address().port);
+});
