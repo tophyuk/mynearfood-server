45eecfb24dc7c6e11b37b4dd9491b702ffd11a4e
상혁 <>
2021-12-07 00:38:35 +0900

    update : router index 추가로 인한 routes 수정

--- routes/user.ts
+++ routes/user.ts
@@ -1,17 +1,16 @@
-module.exports = (app) => {
-  const users = require("../controllers/user.controller");
-  var router = require("express").Router();
+const users = require("../controllers/user.controller");
+const router = require("express").Router();
 
-  // 로그인 및 회원가입
-  router.post("/login", users.login);
-  router.post("/signup", users.singup);
+// 로그인 및 회원가입
+router.post("/login", users.login);
+router.post("/signup", users.singup);
+router.get("/verify", users.verifyToken);
+router.get("/findPassword", users.findPassword);
+// Create a new Tutorial
+router.get("/", users.findAll);
 
-  // Create a new Tutorial
-  router.get("/", users.findAll);
+router.get("/:id", users.findOne);
+router.put("/:id", users.update);
+router.delete("/:id", users.delete);
 
-  router.get("/:id", users.findOne);
-  router.put("/:id", users.update);
-  router.delete("/:id", users.delete);
-
-  app.use("/users", router);
-};
+export default router;
