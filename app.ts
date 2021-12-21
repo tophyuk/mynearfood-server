fafd88d4161696aec52eda85aac8fb36be2ca537
dev_seung2 <dev_seung2@naver.com>
2021-12-22 01:26:35 +0900

    chore: cors 설정 추가

--- app.ts
+++ app.ts
@@ -1,18 +1,25 @@
-import express from "express";
-import router from "./routes";
-const app = express();
+import express from "express"
+import router from "./routes"
+const app = express()
+const cors = require("cors")
 
-const bodyParser = require("body-parser"); // req에 body 태그 변환
-app.use(bodyParser.json());
+const corsOptions = {
+  origin: "http://localhost:9001",
+}
+
+app.use(cors(corsOptions))
+
+const bodyParser = require("body-parser") // req에 body 태그 변환
+app.use(bodyParser.json())
 
 // 변경코드
-app.use("/", router);
+app.use("/", router)
 
 // require("./routes/user")(app);
 
-const PORT = process.env.DB_PORT;
+const PORT = process.env.DB_PORT
 app.listen(3001, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+  console.log(`Server is running on port ${PORT}.`)
+})
 
-export default app;
+export default app
