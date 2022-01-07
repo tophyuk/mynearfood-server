9f019d30d42082e924d55712be4814edd94a87f9
상혁 <>
2022-01-07 23:53:37 +0900

    update : tb_user 날짜 컬럼 삭제로 인한 UserTypes 수정

--- constants/types.ts
+++ constants/types.ts
@@ -5,8 +5,6 @@ export type UsersTypes = {
   nickname: string;
   region: string;
   introduce: string;
-  reg_date: Date;
-  upt_date: Date;
   delete_yn: string;
   login_type: string;
   private_yn: string;
