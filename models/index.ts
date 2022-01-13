e8b71b083cccb51b61087ffcd4dd773726716d32
상혁 <>
2022-01-14 00:48:51 +0900

    update : token 테이블 생성에 따른 수정

--- models/index.ts
+++ models/index.ts
@@ -18,13 +18,34 @@ db.sequelize = sequelize;
 
 db.Users = require("./user")(sequelize, Sequelize);
 db.Profile = require("./profile")(sequelize, Sequelize);
+db.Token = require("./token")(sequelize, Sequelize);
 
 //tb_user -> tb_profiel_image 외래키 참조
 db.Users.hasMany(db.Profile, {
-  foreignKey: "user_id",
+  foreignKey: {
+    name: "user_id",
+    allowNull: false,
+  },
 });
 db.Profile.belongsTo(db.Users, {
-  foreignKey: "user_id",
+  foreignKey: {
+    name: "user_id",
+    allowNull: false,
+  },
+});
+
+//tb_user -> tb_token 외래키 참조
+db.Users.hasMany(db.Token, {
+  foreignKey: {
+    name: "user_id",
+    allowNull: false,
+  },
+});
+db.Token.belongsTo(db.Users, {
+  foreignKey: {
+    name: "user_id",
+    allowNull: false,
+  },
 });
 
 export default db;
