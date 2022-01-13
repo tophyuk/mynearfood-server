476cfa97c97ae6887dc6852881a4e0fe48fb9bdb
상혁 <>
2022-01-14 00:50:47 +0900

    update : 토큰 생성 및 검증

--- routes/user.ts
+++ routes/user.ts
@@ -1,18 +1,19 @@
-const users = require("../controllers/user.controller")
-const router = require("express").Router()
+const users = require("../controllers/user.controller");
+const router = require("express").Router();
 
 // 로그인 및 회원가입
-router.post("/login", users.login)
-router.post("/logout", users.logout)
-router.post("/signup", users.singup)
-router.get("/verify", users.verifyToken)
-router.get("/findPassword", users.findPassword)
-router.get("/checkEmail", users.checkEmail)
-router.get("/checkNickname", users.checkNickname)
+router.post("/login", users.login);
+router.post("/logout", users.logout);
+router.post("/signup", users.singup);
+router.get("/verifyToken", users.verifyToken);
+router.get("/findPassword", users.findPassword);
+router.get("/checkEmail", users.checkEmail);
+router.get("/checkNickname", users.checkNickname);
 
-router.get("/:email", users.findUserByEmail)
-router.get("/:id", users.findOne)
-router.put("/:id", users.update)
-router.delete("/:id", users.delete)
+// router.get("/:email", users.findUserByEmail);
+// router.get("/:id", users.findOne);
 
-export default router
+// router.put("/:id", users.update);
+// router.delete("/:id", users.delete);
+
+export default router;
